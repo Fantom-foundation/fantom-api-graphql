02e4a5357a617d822558015156eb31e0dbdfd7cb
Jiri Malek <jirka.malek@gmail.com>
2020-03-05 21:45:06 +0100

    Optimize the GraphQL schema by removing redundant elements.
    The block query parameters are now optional and if not given, the most recent block is provided by the API server. Sequential access to blocks and transactions using <cursor, count> vector has been extended to allow loading in both directions by means of changing count sign to positive/negative. Corresponding comment was added.

--- internal/graphql/schema/tools/make_bundle.sh
+++ internal/graphql/schema/tools/make_bundle.sh
@@ -10,7 +10,7 @@ BASEFOLDER="$(dirname "$0")/.."
 {
   echo "package $PACKAGE"
   echo ""
-  echo "// Auto generated GraphQL schema bundle; created ", `date "+%F %R"`
+  echo "// Auto generated GraphQL schema bundle; created" `date "+%F %R"`
   echo "const schema = \`"
   find "$BASEFOLDER/definition" -name '*.graphql' -print0 | xargs -0 -I{} sh -c "cat {}; echo ''"
   echo "\`"
