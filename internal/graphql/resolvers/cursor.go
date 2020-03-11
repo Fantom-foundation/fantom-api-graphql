62055bae5c06c0a578d753b819127dddb709110e
Jiri Malek <jirka.malek@gmail.com>
2020-03-11 23:39:51 +0100

    Add GraphQL Cursor scalar implementation.

---
+++ internal/graphql/resolvers/cursor.go
@@ -0,0 +1,33 @@
+package resolvers
+
+import (
+	"errors"
+	"strconv"
+)
+
+// Cursor represents a string key of an element position in a sequential list of edges.
+type Cursor string
+
+// ImplementsGraphQLType notifies the GraphQL that this type resolves Cursor scalar.
+func (Cursor) ImplementsGraphQLType(name string) bool {
+	return name == "Cursor"
+}
+
+// UnmarshalGraphQL unmarshal incoming Cursor into a local variable.
+func (c *Cursor) UnmarshalGraphQL(input interface{}) error {
+	var err error
+	switch input := input.(type) {
+	case string:
+		*c = Cursor(input)
+	case int32:
+		*c = Cursor(strconv.Itoa(int(input)))
+	default:
+		err = errors.New("wrong cursor type")
+	}
+	return err
+}
+
+// MarshalJSON encodes a cursor to JSON for transport.
+func (c Cursor) MarshalJSON() ([]byte, error) {
+	return strconv.AppendQuote(nil, string(c)), nil
+}
