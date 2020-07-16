bebe22030cfa11cd932719efdce9a1719ab0becc
Jiri Malek <jirka.malek@gmail.com>
2020-07-16 22:07:03 +0200

    Add DeFi account repository structure.

---
+++ internal/types/defi_account.go
@@ -0,0 +1,28 @@
+// Package types implements different core types of the API.
+package types
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+)
+
+// DefiAccount represents information about specific DeFi account
+// identified by the owner's address.
+type DefiAccount struct {
+	// address of the DeFi account.
+	Address common.Address
+
+	// Collateral represents a list of all collateral assets on this account.
+	CollateralList []common.Address
+
+	// Debt represents the list of all the borrowed tokens.
+	DebtList []common.Address
+
+	// CollateralValue represents the current collateral value
+	// in ref. denomination (fUSD).
+	CollateralValue hexutil.Big
+
+	// DebtValue represents the current debt value
+	// in ref. denomination (fUSD).
+	DebtValue hexutil.Big
+}
