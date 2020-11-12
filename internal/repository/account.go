16b1d84125020e2ff6b1fc0f7d6f1fbdbfe13eec
Jiri Malek <jirka.malek@gmail.com>
2020-11-12 15:44:50 +0100

    Use off-chain db to load account details where possible.

--- internal/repository/account.go
+++ internal/repository/account.go
@@ -35,19 +35,27 @@ func (p *proxy) Account(addr *common.Address) (*types.Account, error) {
 
 // getAccount builds the account representation after validating it against Lachesis node.
 func (p *proxy) getAccount(addr *common.Address) (*types.Account, error) {
-	// at least we know the account existed
-	acc := types.Account{Address: *addr}
-
-	// check if this is a smart contract account; we log the error on the call
-	acc.ContractTx, _ = p.db.ContractTransaction(addr)
-
-	// also keep a copy at the in-memory cache
-	err := p.cache.PushAccount(&acc)
+	// try to get the account from database first
+	acc, err := p.db.Account(addr)
 	if err != nil {
-		p.log.Warningf("can not keep account [%s] information in memory; %s", addr.Hex(), err.Error())
+		p.log.Errorf("can not get the account %s; %s", addr.String(), err.Error())
+		return nil, err
 	}
 
-	return &acc, nil
+	// found the account in database?
+	if acc == nil {
+		// at least we know the account existed
+		acc := &types.Account{Address: *addr, Type: types.AccountTypeWallet}
+
+		// check if this is a smart contract account; we log the error on the call
+		acc.ContractTx, _ = p.db.ContractTransaction(addr)
+	}
+
+	// also keep a copy at the in-memory cache
+	if err = p.cache.PushAccount(acc); err != nil {
+		p.log.Warningf("can not keep account [%s] information in memory; %s", addr.Hex(), err.Error())
+	}
+	return acc, nil
 }
 
 // AccountBalance returns the current balance of an account at Opera blockchain.
@@ -81,4 +89,37 @@ func (p *proxy) AccountTransactions(acc *types.Account, cursor *string, count in
 // AccountsActive returns total number of accounts known to repository.
 func (p *proxy) AccountsActive() (hexutil.Uint64, error) {
 	return p.db.AccountCount()
+}
+
+// AccountIsKnown checks if the account of the given address is known to the API server.
+func (p *proxy) AccountIsKnown(addr *common.Address) bool {
+	// try cache first
+	stat := p.cache.CheckAccountKnown(addr)
+	if nil != stat {
+		return *stat
+	}
+
+	// check if the database knows the address
+	known, err := p.db.IsAccountKnown(addr)
+	if err != nil {
+		p.log.Errorf("can not check account %s existence; %s", addr.String(), err.Error())
+		return false
+	}
+
+	// if the account is known already, mark it in cache for faster resolving
+	if known {
+		p.cache.PushAccountKnown(addr)
+	}
+	return known
+}
+
+// AccountAdd adds specified account detail into the repository.
+func (p *proxy) AccountAdd(acc *types.Account) error {
+	// add this account to the database and remember it's been added
+	err := p.db.AddAccount(acc)
+	if err == nil {
+		p.cache.PushAccountKnown(&acc.Address)
+	}
+
+	return err
 }
