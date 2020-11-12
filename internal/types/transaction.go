3c7df9a587c4d686177506ca310eecb2b506b361
Jiri Malek <jirka.malek@gmail.com>
2020-11-12 15:49:22 +0100

    Add transactions ordinal index support.

--- internal/types/transaction.go
+++ internal/types/transaction.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"math/rand"
 )
 
 // Transaction represents a basic information provided by the API about transaction inside Opera blockchain.
@@ -56,6 +57,24 @@ type Transaction struct {
 
 	// Status represents transaction status; value is either 1 (success) or 0 (failure)
 	Status *hexutil.Uint64 `json:"status"`
+}
+
+// mustTransactionIndex always calculate the index of the current transaction
+func TransactionIndex(block *Block, trx *Transaction) uint64 {
+	// what is the transaction index
+	var txIndex uint64
+	if trx.TrxIndex != nil {
+		txIndex = uint64(*trx.TrxIndex)
+	} else {
+		txIndex = uint64((rand.Uint32()<<10)|0xff) & 0x3fff
+	}
+	return trxOrdinalIndex(uint64(block.Number), txIndex)
+}
+
+// trxOrdinalIndex calculates ordinal index in the whole blockchain.
+// This gives us about 700 years of index space with 50k blocks per second rate + 10 years to fix than.
+func trxOrdinalIndex(block uint64, trxIndex uint64) uint64 {
+	return (block << 14) | trxIndex
 }
 
 // UnmarshalTransaction parses the JSON-encoded block data.
