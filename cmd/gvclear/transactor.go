b1ad5e5542013fdc4445fd7b9094cea04f2c8c79
Jiri Malek <jirka.malek@gmail.com>
2020-12-13 14:42:03 +0100

    Add transactor utility functions for call opts.

---
+++ cmd/gvclear/transactor.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"math/big"
+)
+
+// defaultCallOpts creates a default record for call options.
+func defaultCallOpts(adr *common.Address, con *ethclient.Client) *bind.CallOpts {
+	return &bind.CallOpts{
+		Pending:     false,
+		From:        *adr,
+		BlockNumber: mustBlockHeight(con),
+		Context:     context.Background(),
+	}
+}
+
+// mustBlockHeight returns the current block height
+// of the block chain. It returns nil if the block height can not be pulled.
+func mustBlockHeight(con *ethclient.Client) *big.Int {
+	// get the current block number
+	blk, err := con.BlockNumber(context.Background())
+	if err != nil {
+		fmt.Printf("Con not get the current block height!\n%s", err.Error())
+		return nil
+	}
+
+	return new(big.Int).SetUint64(blk)
+}
