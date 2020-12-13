28d0ec812647f35f95320a1ddd7a5fad75d2ef7c
Jiri Malek <jirka.malek@gmail.com>
2020-12-13 14:41:03 +0100

    Add keystore related configuration.

--- cmd/gvclear/config.go
+++ cmd/gvclear/config.go
@@ -1,4 +1,4 @@
-package gvclear
+package main
 
 import "flag"
 
@@ -8,6 +8,8 @@ type gvConf struct {
 	isVersionQuery bool
 	nodeRpcUrl     string
 	govAddress     string
+	sigKeyFile     string
+	sigPasswd      string
 }
 
 // config reads the Governance Clear app command parameters
@@ -20,6 +22,8 @@ func config() *gvConf {
 	flag.BoolVar(&cfg.isVersionQuery, "version", false, "get the application version")
 	flag.StringVar(&cfg.nodeRpcUrl, "rpc", "lachesis.ipc", "URL of the Opera node RPC/IPC interface")
 	flag.StringVar(&cfg.govAddress, "gov", "", "governance contract address")
+	flag.StringVar(&cfg.sigKeyFile, "key", "", "path to the account key file")
+	flag.StringVar(&cfg.sigPasswd, "pwd", "", "account key file password")
 
 	// parse the flags
 	flag.Parse()
