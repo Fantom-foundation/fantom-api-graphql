5d5ae4743970d799f8674c4bfb599a967e86ba4a
Jiri Malek <jirka.malek@gmail.com>
2021-04-01 17:44:38 +0200

    Update validator deactivation refs to match API.

--- internal/types/validator.go
+++ internal/types/validator.go
@@ -8,12 +8,12 @@ import (
 
 // Validator represents a validator information.
 type Validator struct {
-	Id               hexutil.Big     `json:"id"`
-	StakerAddress    common.Address  `json:"address"`
-	TotalStake       *hexutil.Big    `json:"totalStake"`
-	Status           hexutil.Uint64  `json:"status"`
-	CreatedEpoch     hexutil.Uint64  `json:"createdEpoch"`
-	CreatedTime      hexutil.Uint64  `json:"createdTime"`
-	DeactivatedEpoch *hexutil.Uint64 `json:"deactivatedEpoch"`
-	DeactivatedTime  *hexutil.Uint64 `json:"deactivatedTime"`
+	Id               hexutil.Big    `json:"id"`
+	StakerAddress    common.Address `json:"address"`
+	TotalStake       *hexutil.Big   `json:"totalStake"`
+	Status           hexutil.Uint64 `json:"status"`
+	CreatedEpoch     hexutil.Uint64 `json:"createdEpoch"`
+	CreatedTime      hexutil.Uint64 `json:"createdTime"`
+	DeactivatedEpoch hexutil.Uint64 `json:"deactivatedEpoch"`
+	DeactivatedTime  hexutil.Uint64 `json:"deactivatedTime"`
 }
