e8b1c0845f482de74c038243d98f4c18ec259eec
Jiri Malek <jirka.malek@gmail.com>
2021-04-28 18:42:36 +0200

    Add compatibility with the SFCv2 rewards.

--- internal/types/pending_rewards.go
+++ internal/types/pending_rewards.go
@@ -12,3 +12,21 @@ type PendingRewards struct {
 	Staker  hexutil.Big
 	Amount  hexutil.Big
 }
+
+// FromEpoch returns the first unpaid epoch.
+// This is deprecated in SFCv3.
+func (pr PendingRewards) FromEpoch() hexutil.Uint64 {
+	return 0
+}
+
+// ToEpoch returns the last unpaid epoch.
+// This is deprecated in SFCv3.
+func (pr PendingRewards) ToEpoch() hexutil.Uint64 {
+	return 0
+}
+
+// IsOverRange returns if the pending reward is over safe range.
+// This is deprecated in SFCv3.
+func (pr PendingRewards) IsOverRange() bool {
+	return false
+}
