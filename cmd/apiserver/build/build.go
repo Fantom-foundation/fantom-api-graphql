41ca7b275d063f41e884a895a2b64776f8d95775
Jiri Malek <jirka.malek@gmail.com>
2021-07-27 13:44:52 +0200

    Add descriptions to global build variables.

--- cmd/apiserver/build/build.go
+++ cmd/apiserver/build/build.go
@@ -10,13 +10,25 @@ import (
 	"runtime"
 )
 
+// Version represents the version of the app.
 var Version = "undefined"
+
+// Commit represents the GitHub commit hash the app was built from.
 var Commit = "undefined"
+
+// CommitTime represents the GitHub commit time stamp the app was built from.
 var CommitTime = "undefined"
+
+// Time represents the time of the app build.
 var Time = "undefined"
+
+// Compiler represents the information about the compiler used to build the app.
 var Compiler = "undefined"
 
+// Reset represents a token for terminal color reset.
 var Reset = "\033[0m"
+
+// Blue represents a token for blue terminal color setup.
 var Blue = "\033[34m"
 
 // init initializes the build reference on the given OS
