a4b1057bfcb419df95efbce680c4ba74f65320ff
Jiri Malek <jirka.malek@gmail.com>
2021-08-04 15:18:34 +0200

    Clean up the application start.

---
+++ cmd/apiserver/app.go
@@ -0,0 +1,155 @@
+// Package main implements the API server entry point.
+package main
+
+import (
+	"fantom-api-graphql/cmd/apiserver/build"
+	"fantom-api-graphql/internal/config"
+	"fantom-api-graphql/internal/graphql/resolvers"
+	"fantom-api-graphql/internal/handlers"
+	"fantom-api-graphql/internal/logger"
+	"fantom-api-graphql/internal/repository"
+	"fantom-api-graphql/internal/svc"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+// apiServer implements the API server application
+type apiServer struct {
+	cfg          *config.Config
+	log          logger.Logger
+	api          resolvers.ApiResolver
+	srv          *http.Server
+	isVersionReq bool
+}
+
+// init initializes the API server
+func (app *apiServer) init() {
+	// make sure to capture version request and rescan depth
+	flag.BoolVar(&app.isVersionReq, "v", false, "get the application version")
+
+	// get the configuration including parsing the calling flags
+	var err error
+	app.cfg, err = config.Load()
+	if nil != err {
+		log.Fatal(err)
+		return
+	}
+
+	// configure logger based on the configuration
+	app.log = logger.New(app.cfg)
+
+	// make sure to pass logger and config to internals
+	repository.SetConfig(app.cfg)
+	repository.SetLogger(app.log)
+	svc.SetConfig(app.cfg)
+	svc.SetLogger(app.log)
+
+	// make the HTTP server
+	app.makeHttpServer()
+}
+
+// run executes the API server function.
+func (app *apiServer) run() {
+	// print the app version and exit if this is the only thing requested
+	build.PrintVersion(app.cfg)
+	if app.isVersionReq {
+		return
+	}
+
+	// make sure to capture terminate signals
+	app.observeSignals()
+
+	// start responding to requests
+	app.log.Infof("welcome to Fantom GraphQL API server")
+	app.log.Infof("listening for requests on %s", app.cfg.Server.BindAddress)
+	log.Fatal(app.srv.ListenAndServe())
+}
+
+// makeHttpServer creates and configures the HTTP server to be used to serve incoming requests
+func (app *apiServer) makeHttpServer() {
+	// create request MUXer
+	srvMux := new(http.ServeMux)
+
+	// create HTTP server to handle our requests
+	app.srv = &http.Server{
+		Addr:              app.cfg.Server.BindAddress,
+		ReadTimeout:       time.Second * time.Duration(app.cfg.Server.ReadTimeout),
+		WriteTimeout:      time.Second * time.Duration(app.cfg.Server.WriteTimeout),
+		IdleTimeout:       time.Second * time.Duration(app.cfg.Server.IdleTimeout),
+		ReadHeaderTimeout: time.Second * time.Duration(app.cfg.Server.HeaderTimeout),
+		Handler:           srvMux,
+	}
+
+	// setup handlers
+	app.setupHandlers(srvMux)
+}
+
+// setupHandlers initializes an array of handlers for our HTTP API end-points.
+func (app *apiServer) setupHandlers(mux *http.ServeMux) {
+	// create root resolver
+	app.api = resolvers.New(app.cfg, app.log)
+
+	// setup GraphQL API handler
+	h := http.TimeoutHandler(
+		handlers.Api(app.cfg, app.log, app.api),
+		time.Second*time.Duration(app.cfg.Server.ResolverTimeout),
+		"Service timeout.",
+	)
+	mux.Handle("/api", h)
+	mux.Handle("/graphql", h)
+
+	// setup gas price estimator REST API resolver
+	mux.Handle("/json/gas", handlers.GasPrice(app.log))
+
+	// handle GraphiQL interface
+	mux.Handle("/graphi", handlers.GraphiHandler(app.cfg.Server.DomainAddress, app.log))
+}
+
+// observeSignals setups terminate signals observation.
+func (app *apiServer) observeSignals() {
+	// log what we do
+	app.log.Info("os signals captured")
+
+	// make the signal consumer
+	ts := make(chan os.Signal, 1)
+	signal.Notify(ts, syscall.SIGINT, syscall.SIGTERM)
+
+	// start monitoring
+	go func() {
+		// wait for the signal
+		<-ts
+		app.terminate()
+
+		// we are done
+		app.log.Info("api server done")
+		os.Exit(0)
+	}()
+}
+
+// terminate modules of the API server.
+func (app *apiServer) terminate() {
+	app.log.Notice("api server terminates")
+
+	// terminate responder
+	if err := app.srv.Close(); err != nil {
+		app.log.Errorf("could not terminate HTTP listener")
+	}
+
+	// terminate services; this closes observers, scanners and dispatchers
+	if mgr := svc.Manager(); mgr != nil {
+		mgr.Close()
+	}
+
+	// terminate repository; this closes connections
+	if repo := repository.R(); repo != nil {
+		repo.Close()
+	}
+
+	// close resolvers
+	app.api.Close()
+}
--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,22 +1,9 @@
+// Package main implements the API server entry point.
 package main
 
 import (
-	"fantom-api-graphql/cmd/apiserver/build"
-	"fantom-api-graphql/internal/config"
-	"fantom-api-graphql/internal/graphql/resolvers"
-	"fantom-api-graphql/internal/handlers"
-	"flag"
 	"net/http"
 	"time"
-
-	/* "fantom-api-graphql/internal/handlers" */
-	"fantom-api-graphql/internal/logger"
-	"fantom-api-graphql/internal/repository"
-	"log"
-	/* "net/http" */
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 // init initializes the package and sets some important app-wide options
@@ -31,102 +18,7 @@ func init() {
 
 // main initializes the API server and starts it when ready.
 func main() {
-	// make sure to capture version request and rescan depth
-	versionRequest := flag.Bool("v", false, "get the application version")
-
-	// get the configuration to prepare the server
-	cfg, err := config.Load()
-	if nil != err {
-		log.Fatal(err)
-	}
-
-	// print the version information
-	build.PrintVersion(cfg)
-	if *versionRequest {
-		return
-	}
-
-	// make logger
-	lg := logger.New(cfg)
-
-	// create repository for data exchange with the blockchain full node and local persistent storage
-	repository.SetConfig(cfg)
-	repository.SetLogger(lg)
-
-	// start the HTTP server
-	startHttpServer(cfg, lg)
-}
-
-// startHttpServer starts the HTTP server and begins resolving incoming requests.
-func startHttpServer(cfg *config.Config, log logger.Logger) {
-	// create request MUXer
-	srvMux := new(http.ServeMux)
-
-	// create a HTTP server to handle our requests
-	srv := &http.Server{
-		Addr:              cfg.Server.BindAddress,
-		ReadTimeout:       time.Second * time.Duration(cfg.Server.ReadTimeout),
-		WriteTimeout:      time.Second * time.Duration(cfg.Server.WriteTimeout),
-		IdleTimeout:       time.Second * time.Duration(cfg.Server.IdleTimeout),
-		ReadHeaderTimeout: time.Second * time.Duration(cfg.Server.HeaderTimeout),
-		Handler:           srvMux,
-	}
-
-	// capture termination signals and start listening
-	setupSignals(setupHandlers(srvMux, cfg, log), log)
-
-	// log the server opening info
-	log.Infof("welcome to Fantom GraphQL API server network interface.")
-	log.Infof("listening for requests on [%s]", cfg.Server.BindAddress)
-
-	// start the server
-	log.Fatal(srv.ListenAndServe())
-}
-
-// setupHandlers initializes an array of handlers for our HTTP API end-points.
-func setupHandlers(mux *http.ServeMux, cfg *config.Config, log logger.Logger) resolvers.ApiResolver {
-	// create root resolver
-	rs := resolvers.New(cfg, log)
-	log.Notice("initialized, going live")
-
-	// setup GraphQL API handler
-	h := http.TimeoutHandler(handlers.Api(cfg, log, rs), time.Second*time.Duration(cfg.Server.ResolverTimeout), "Service timeout.")
-	mux.Handle("/api", h)
-	mux.Handle("/graphql", h)
-
-	// setup REST API
-	mux.Handle("/json/gas", handlers.GasPrice(log))
-
-	// handle GraphiQL interface
-	mux.Handle("/graphi", handlers.GraphiHandler(cfg.Server.DomainAddress, log))
-	return rs
-}
-
-// setupSignals creates a system signal listener and handles graceful termination upon receiving one.
-func setupSignals(rs resolvers.ApiResolver, log logger.Logger) {
-	// log what we do
-	log.Info("os signals captured")
-
-	// make the signal consumer
-	ts := make(chan os.Signal, 1)
-	signal.Notify(ts, syscall.SIGINT, syscall.SIGTERM)
-
-	// start monitoring
-	go func() {
-		// wait for the signal
-		<-ts
-
-		// log nad close repository
-		log.Notice("server is terminating")
-		if repo := repository.R(); repo != nil {
-			repo.Close()
-		}
-
-		// close resolvers
-		rs.Close()
-
-		// we are done
-		log.Info("done")
-		os.Exit(0)
-	}()
+	app := apiServer{}
+	app.init()
+	app.run()
 }
