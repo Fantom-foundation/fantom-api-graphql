e8c3e1da56c8665f9cab3a5e1655e5cbfbded634
Jiri Malek <jirka.malek@gmail.com>
2021-09-24 03:07:22 +0200

    Use local pre-compiled ABIgen for contracts interface.

--- internal/repository/rpc/tools/abigen.sh
+++ internal/repository/rpc/tools/abigen.sh
@@ -7,26 +7,14 @@
 # 2) run "go install ./cmd/abigen" (or "make devtools")
 # 3) the abigen tool should be now installed in $GOPATH
 #
+if [ -z "${GOPATH}" ]; then GOPATH="${HOME}/go"; fi
+ABIGEN="${GOPATH}/bin/abigen"
 
-if [ -x "${GOPATH}"/bin/abigen ]
+if [ ! -x "${ABIGEN}" ]
 then
-  "${GOPATH}"/bin/abigen "${@}"
-  exit $?
+  echo "ABI generator not found as [${ABIGEN}]."
+  exit 1
 fi
 
-BASE_FOLDER=$( cd "$(dirname "$0")"/../../../.. && pwd)
-OS=$(uname)
-case $OS in
-'Linux')
-  "${BASE_FOLDER}"/tools/bin/abigen-linux-amd64 "${@}"
-  exit $?
-  ;;
-'Darwin')
-  "${BASE_FOLDER}"/tools/bin/abigen-darwin-arm64 "${@}"
-  exit $?
-  ;;
-*)
-  echo "Can not determine the right ABI generator tool."
-  exit 1
-  ;;
-esac
+"${ABIGEN}" "${@}"
+exit $?
