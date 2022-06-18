9e88ef197416940c26efe6a60889144ac03fbc4f
mike <pelanek.lukas@seznam.cz>
2022-06-18 22:09:12 +0200

    refactor: epoch end time as uint64

--- internal/types/epoch.go
+++ internal/types/epoch.go
@@ -3,16 +3,13 @@ package types
 
 import (
 	"encoding/json"
-	"fantom-api-graphql/internal/repository/db/registry"
 	"github.com/ethereum/go-ethereum/common/hexutil"
-	"time"
 )
 
 // Epoch represents epoch detail.
 type Epoch struct {
 	Id                    hexutil.Uint64 `json:"id"  bson:"_id"`
-	EndTime               hexutil.Uint64 `json:"end" bson:"et"`
-	End                   time.Time      `bson:"end"`
+	EndTime               uint64         `json:"end" bson:"end"`
 	EpochFee              hexutil.Big    `json:"fee" bson:"fee"`
 	TotalBaseRewardWeight hexutil.Big    `json:"trw" bson:"brw"`
 	TotalTxRewardWeight   hexutil.Big    `json:"txw" bson:"trw"`
@@ -31,12 +28,4 @@ func UnmarshalEpoch(data []byte) (*Epoch, error) {
 // Marshal returns the JSON encoding of Epoch.
 func (e *Epoch) Marshal() ([]byte, error) {
 	return json.Marshal(e)
-}
-
-// MarshalBSON creates a BSON representation of the Epoch record.
-func (e *Epoch) MarshalBSON() ([]byte, error) {
-	e.End = time.Unix(int64(e.EndTime), 0)
-
-	// prep the structure for saving
-	return registry.Marshal(e)
 }
